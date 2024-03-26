4a497e2d7bf5b3f3934d2498d24b0cbba31c62df
monkey1739 <102886260+monkey1739@users.noreply.github.com>
2024-03-25 20:24:34 -0700

    Vista de consulta general (le falta)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
+import "@/styles/scrollbar.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
