f93666d1f8032051edfa2a4043a693819e8a6c76
Luis Vega <HonorVex03@gmail.com>
2024-04-09 15:50:55 -0700

    Rediseño de la página principal
    Rediseño de la página principal, se creó el nuevo SVG y se modificó la paleta de colores en las tarjetas, aparte de hacerle modificaciones a la forma del wave.

--- src/components/sucursal/Tarjeta.tsx
+++ src/components/sucursal/Tarjeta.tsx
@@ -1,86 +1,94 @@
-import Image from 'next/image'
-import Alumnos from '@/img/T1A.png'
-import Docentes from '@/img/T2D.png'
-import Cursos from '@/img/T3C.png'
-import Sucursales from '@/img/T4S.png'
-import { Sucursal } from '@prisma/client'
-import { toTitleCase } from '@/lib/utils'
-import Link from 'next/link'
+import Image from "next/image";
+import Alumnos from "@/img/T1A.png";
+import Docentes from "@/img/T2D.png";
+import Cursos from "@/img/T3C.png";
+import Sucursales from "@/img/T4S.png";
+import { Sucursal } from "@prisma/client";
+import { toTitleCase } from "@/lib/utils";
+import Link from "next/link";
 
 const datos = [
-	{
-		id: 1,
-		img: Alumnos,
-		titulo: 'Alumnos',
-		desc: 'Gestión de alumnos',
-		href: 'alumnos'
-	},
-	{
-		id: 2,
-		img: Docentes,
-		titulo: 'Docentes',
-		desc: 'Gestión de docentes',
-		href: 'docentes'
-	},
-	{
-		id: 3,
-		img: Cursos,
-		titulo: 'Cursos',
-		desc: 'Gestión de clases',
-		href: 'cursos'
-	},
-	{
-		id: 4,
-		img: Sucursales,
-		titulo: 'Sucursal',
-		desc: 'Información y gastos',
-		href: 'informacion'
-	}
-]
+  {
+    id: 1,
+    img: Alumnos,
+    titulo: "Alumnos",
+    desc: "Gestión de alumnos",
+    href: "alumnos",
+  },
+  {
+    id: 2,
+    img: Docentes,
+    titulo: "Docentes",
+    desc: "Gestión de docentes",
+    href: "docentes",
+  },
+  {
+    id: 3,
+    img: Cursos,
+    titulo: "Cursos",
+    desc: "Gestión de clases",
+    href: "cursos",
+  },
+  {
+    id: 4,
+    img: Sucursales,
+    titulo: "Sucursal",
+    desc: "Información y gastos",
+    href: "informacion",
+  },
+];
 
 const Tarjeta = ({ sucursal }: { sucursal: Sucursal }) => {
-	return (
-		<div className="rounded-2xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 place-items-center gap-6">
-			{datos.map(({ id, img, titulo, desc, href }) => {
-				return (
-					<Link
-						href={`/sucursal/${sucursal.id}/${href}`}
-						key={id}
-						className="group bg-gradient-to-b from-pink-400 to-pink-900 rounded-2xl cursor-pointer"
-					>
-						<div className="relative w-[250px] max-w-[250px] h-[300px] overflow-hidden rounded-[inherit] m-[2px]">
-							<Image
-								alt={`Imagen que muestra ${desc} de la sucursal ${toTitleCase(
-									sucursal.nombre
-								)}`}
-								fill={true}
-								className="block object-cover transition-transform group-hover:scale-105"
-								src={img}
-							/>
-							<Image
-								alt="Figura de onda que decora la tarjeta de menú de la sucursal"
-								aria-hidden={true}
-								width={100}
-								height={100}
-								className="object-cover absolute w-full right-0 -bottom-14 translate-y-0 transition-transform group-hover:-translate-y-5"
-								src="/wave-tarjeta-menu.svg"
-							/>
-							<div className="absolute bottom-0 left-0 right-0 pb-8">
-								<h3 className="text-white text-center font-bold text-3xl">
-									{titulo}
-								</h3>
-								<div className="grid grid-rows-[0fr] group-hover:grid-rows-[1fr] transition-[grid-template-rows]">
-									<p className="text-white text-center overflow-hidden group-hover:opacity-100  transition-opacity">
-										{desc}
-									</p>
-								</div>
-							</div>
-						</div>
-					</Link>
-				)
-			})}
-		</div>
-	)
-}
+  return (
+    <div className="rounded-2xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 place-items-center gap-6">
+      {datos.map(({ id, img, titulo, desc, href }) => {
+        return (
+          <Link
+            href={`/sucursal/${sucursal.id}/${href}`}
+            key={id}
+            className="group bg-gradient-to-b from-black to-white rounded-2xl cursor-pointer"
+          >
+            <div className="relative w-[250px] max-w-[250px] h-[300px] overflow-hidden rounded-[inherit] m-[2px]">
+              <Image
+                alt={`Imagen que muestra ${desc} de la sucursal ${toTitleCase(
+                  sucursal.nombre
+                )}`}
+                fill={true}
+                className="block object-cover transition-transform group-hover:scale-105"
+                src={img}
+              />
+              <Image
+                alt="Figura de onda que decora la tarjeta de menú de la sucursal"
+                aria-hidden={true}
+                width={1000}
+                height={1000}
+                className="object-cover absolute w-full right-0 -bottom-14 translate-y-0 transition-transform group-hover:-translate-y-5 filter blur-sm mix-blend-multiply"
+                src="/wave-tarjeta-menu.svg"
+              />
+              <Image
+                alt="Figura de onda que decora la tarjeta de menú de la sucursal"
+                aria-hidden={true}
+                width={1000}
+                height={1000}
+                className="object-cover absolute w-full right-0 -bottom-14 translate-y-0 transition-transform group-hover:-translate-y-5 filter opacity-80"
+                src="/wave-tarjeta-menu-bg.svg"
+              />
+              <div className="absolute bottom-0 left-0 right-0 pb-8">
+                <h3 className="text-white text-center font-bold text-3xl">
+                  {titulo}
+                </h3>
+                <div className="grid grid-rows-[0fr] group-hover:grid-rows-[1fr] transition-[grid-template-rows]">
+                  <p className="text-white text-center overflow-hidden group-hover:opacity-100  transition-opacity">
+                    {desc}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
 
-export default Tarjeta
+export default Tarjeta;
