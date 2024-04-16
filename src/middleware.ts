4033fbb6be7ef9ff9e989d33a7a99724da74147f
Stiven <s1182579@uabc.edu.mx>
2024-04-15 23:45:57 -0700

    Permitir registrar usuarios sin iniciar sesión
    Middleware modificado para permitir hacer peticiones a la api cuando se quiera registrar un nuevo usuario sin haber iniciado sesión.

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,5 +5,5 @@ export const config = {
 	 * Las rutas protegidas por la sesión de usuario serán
 	 * todas las que no sean registro o login
 	 **/
-	matcher: ['/((?!registro|login).*)']
+	matcher: ['/((?!registro|login|api/usuario).*)']
 }
